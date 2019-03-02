18d5455b52c4ceceb63adacb43ae49994de1bce3
thadhaines <jthaines@mtech.edu>
2019-03-02 14:26:08 -0700

    Initial rabbitMQ example

---
+++ recieve.py
@@ -0,0 +1,16 @@
+import pika
+
+#NOTE: for ironpython not to err - use 127.0.0.1 instead of localhost 
+#Seems to connect to the same as localhost anyway
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+channel.basic_consume(callback, queue='hello', no_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
